912b68c71caeab5d0dc372098f392087892171be
Ben <bensei09@gmail.com>
2020-11-16 19:39:48 +0100

    ahora tiene codigo para salir

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var num1 = prompt('Introduzca el primer número: ');
+var num2 = prompt('Introduzca el segundo número: ');
+var num3 = prompt('Introduzca el tercero número: ');
+var num4 = prompt('Introduzca el cuarto número: ');
+var suma = parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4);
+
+document.write('La suma en total de los números ' + num1 
++ ' + ' + num2 + ' + ' + num3 + ' + git' + num4 + ' + ' + '= ' + suma);
